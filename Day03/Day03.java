2430836036c90aaff8a70163d2b8e099f346e79a
Brayden <coghill30@gmail.com>
2023-12-04 15:31:29 -0800

    In progress

--- Day03/Day03.java
+++ Day03/Day03.java
@@ -6,6 +6,7 @@ import AdventUtilities.*;
 public class Day03{
 
 public class Main {
+    
 public static void main(String[] args) throws IOException {
 	ArrayList<String> input = AdventUtilities.readLines("Day03/Day03.txt");
 	System.out.println(sumPartNumbers(input));
