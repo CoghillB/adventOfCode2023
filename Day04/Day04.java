d8cba0b939f3f91644775a0e0ee055f265e252da
Brayden <coghill30@gmail.com>
2023-12-04 14:56:57 -0800

    In Progress

--- Day04/Day04.java
+++ Day04/Day04.java
@@ -2,15 +2,33 @@ package Day04;
 import java.util.*;
 import java.io.IOException;
 import AdventUtilities.*;
-
+// program runs through lottery ticket and checks it against the winning numbers. If any of the numbers match the winning
+// numbers, the program returns the number of matching numbers
 public class Day04{
 	public static void main(String[] args) throws IOException {
 		ArrayList<String> input = AdventUtilities.readLines("Day04/Day04.txt");
-	}
-}
+		int valid = 0;
+		for (String line : input) {
+			if (isValid(line)) {
+				valid++;
+			}
+		}
+		System.out.println(valid);	
 
-public class Day04Part2 {
-	public static void main(String[] args) throws IOException {
-		ArrayList<String> input = AdventUtilities.readLines("Day04/Day04.txt");
+	}
+
+	public static boolean isValid(String line) {
+		String[] fields = line.split(" ");
+		HashSet<String> fieldNames = new HashSet<String>();
+		for (String field : fields) {
+			fieldNames.add(field.split(":")[0]);
+		}
+		if (fieldNames.size() == 8) {
+			return true;
+		}
+		if (fieldNames.size() == 7 && !fieldNames.contains("cid")) {
+			return true;
+		}
+		return false;
 	}
 }
