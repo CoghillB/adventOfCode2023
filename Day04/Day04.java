a001b50417dff3ba7451e4b761fd753f57f648d3
Brayden <coghill30@gmail.com>
2023-12-04 15:29:05 -0800

    Part1 - complete

--- Day04/Day04.java
+++ Day04/Day04.java
@@ -2,33 +2,35 @@ package Day04;
 import java.util.*;
 import java.io.IOException;
 import AdventUtilities.*;
-// program runs through lottery ticket and checks it against the winning numbers. If any of the numbers match the winning
-// numbers, the program returns the number of matching numbers
-public class Day04{
+/* checks each cards numbers against winning numbers and returns how many numbers matched on each line
+ * then adds all the numbers together and returns the total
+ */
+public class Day04 {
 	public static void main(String[] args) throws IOException {
-		ArrayList<String> input = AdventUtilities.readLines("Day04/Day04.txt");
-		int valid = 0;
-		for (String line : input) {
-			if (isValid(line)) {
-				valid++;
+	ArrayList<String> input = AdventUtilities.readLines("C:\\Users\\coghi\\OneDrive\\Documents\\AdventOfCode\\adventOfCode2023\\Day04\\Day04.txt");
+	int total = 0;
+	for (String line : input) {
+		String[] parts = line.split(":");
+		String[] numbersParts = parts[1].trim().split("\\|");
+		String[] winningNumbersStr = numbersParts[0].trim().split(" ");
+		String[] yourNumbersStr = numbersParts[1].trim().split(" ");
+		Set<Integer> winningNumbers = new HashSet<>();
+		for (String num : winningNumbersStr) {
+			if (!num.isEmpty()) {
+				winningNumbers.add(Integer.parseInt(num));
 			}
 		}
-		System.out.println(valid);	
-
+		Set<Integer> yourNumbers = new HashSet<>();
+		for (String num : yourNumbersStr) {
+			if (!num.isEmpty()) {
+				yourNumbers.add(Integer.parseInt(num));
+			}
+		}
+		Set<Integer> intersection = new HashSet<>(winningNumbers);
+		intersection.retainAll(yourNumbers);
+		int score = intersection.size() > 0 ? (int) Math.pow(2, intersection.size() - 1) : 0;
+		total += score;
 	}
-
-	public static boolean isValid(String line) {
-		String[] fields = line.split(" ");
-		HashSet<String> fieldNames = new HashSet<String>();
-		for (String field : fields) {
-			fieldNames.add(field.split(":")[0]);
-		}
-		if (fieldNames.size() == 8) {
-			return true;
-		}
-		if (fieldNames.size() == 7 && !fieldNames.contains("cid")) {
-			return true;
-		}
-		return false;
+	System.out.println(total);
 	}
 }
